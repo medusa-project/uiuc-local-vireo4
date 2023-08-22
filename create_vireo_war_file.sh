921c9bbd0a23eca37f83ea7ea1c3222d4f852935
Seth Robbins <srobbins@illinois.edu>
2023-08-22 17:46:31 -0500

    Make sure maven uses the correct java version.

--- create_vireo_war_file.sh
+++ create_vireo_war_file.sh
@@ -5,7 +5,7 @@
 echo "From create_vireo_war_file.sh, the current folder is" `pwd`
 
 # now run maven
-mvn clean package -Dproduction -Dassets.uri=file:/home/vireo/vireo4_data/assets
+JAVA_HOME='/home/vireo/java/jdk-11.0.1' mvn clean package -Dproduction -Dassets.uri=file:/home/vireo/vireo4_data/assets
 
 
 
